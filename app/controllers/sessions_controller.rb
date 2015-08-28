3e57d2eca0aacbd5773891ec0665ad69e85713c9
reyes rodriguez rodriguez <reyesorodriguez@gmail.com>
2015-08-28 16:18:48 -0400

    user login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       # direct to profile page
-      
+       redirect_to user
     else
       # create error msg
       render new
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,28 @@
 class UsersController < ApplicationController
+ before_action :authenticate, except: [:index]
+	def index
+		@users = User.all
+	end
+
 	def new
-  @user = User.new
+  	@user = User.new
 	end
 
 	def create
 		@user = User.create(user_params)
   		if @user.save
-  			session[:user_id] = @user_id
-	  			redirect_to  user_post
+  			# session[:user_id] = @user_id
+	  			redirect_to  user_path
 	  			else
 					render template: "sessions/new"
+					render new
   		end
   	end
-  
-
+  	
+  def show
+  	@user = User.find(params[:id])
+  	@tasks = @user.tasks
+  end
   
 
 private
