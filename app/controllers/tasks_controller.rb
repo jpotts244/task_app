b7891945ec8ff4d71ab6f5d4c444324610bde34c
Viktoria Szontagh <vikkiszontagh@gmail.com>
2015-08-31 10:11:04 -0400

    vendor files removed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -53,7 +53,7 @@ class TasksController < ApplicationController
 
   private
   def task_params
-    params.require(:task).permit(:title, :content, :duedate, :location)
+    params.require(:task).permit(:title, :content, :duedate, :location, :image)
   end
 
   # def tasking_params
