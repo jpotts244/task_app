2238ad946ed007c88b487926139c111c08aa3898
Xi Zhang <xizhangmit@gmail.com>
2015-08-31 17:57:14 -0400

    added google map

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -2,26 +2,3 @@
 
 // console.log("linked")
 
-$(function(){
-
-	$("#showList").on("click",function(event){
-		// $("#showList").off("click");
-		$("#map-wrapper").hide();
-		$("#listDisplay").show();
-		$(this).removeClass("btn btn-link");
-		$("#showMap").addClass("btn btn-link");
-
-	});
-
-	$("#showMap").on("click",function(event){
-
-		// $("#showMap").off("click");
-		$("#listDisplay").hide();
-		$("#map-wrapper").show();
-		$("#map").show();
-		$(this).removeClass("btn btn-link");
-		$("#showList").addClass("btn btn-link");			
-
-	});
-
-})
