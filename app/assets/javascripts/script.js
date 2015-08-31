cd3ef8928dc604ef4ea6d908e0ad81f98c8f03c2
Xi Zhang <xizhangmit@gmail.com>
2015-08-31 13:37:45 -0400

    init map

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,3 +1,27 @@
 // this is file for customizing JS
 
 // console.log("linked")
+
+$(function(){
+
+	$("#showList").on("click",function(event){
+		// $("#showList").off("click");
+		$("#map-wrapper").hide();
+		$("#listDisplay").show();
+		$(this).removeClass("btn btn-link");
+		$("#showMap").addClass("btn btn-link");
+
+	});
+
+	$("#showMap").on("click",function(event){
+
+		// $("#showMap").off("click");
+		$("#listDisplay").hide();
+		$("#map-wrapper").show();
+		$("#map").show();
+		$(this).removeClass("btn btn-link");
+		$("#showList").addClass("btn btn-link");			
+
+	});
+
+})
--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,7 @@ Rails.application.routes.draw do
 
 
   # root 'welcome#index'
-
-
-   root 'welcome#index'
-
- 
-
-  # root 'welcome#index'
+  
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
